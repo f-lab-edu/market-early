9fa4027229188122cffa8601ebdceacb50a4ecfe
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-04-03 20:57:09 +0900

    [#2] UserLoginDTO 사용성에 대한 주석

--- src/main/java/kr/flap/domain/model/user/dto/UserLoginDTO.java
+++ src/main/java/kr/flap/domain/model/user/dto/UserLoginDTO.java
@@ -8,13 +8,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 로그인 정보를 담은 DTO.
+ */
 @Getter
 @AllArgsConstructor
 public class UserLoginDTO {
 
+  /* 로그인에 필요한 이메일 주소 */
+  @NotBlank(message = "이메일을 입력해주세요.")
   @Email(message = "이메일 형식을 맞춰주세요.")
   private String email;
 
+  /* 로그인에 필요한 비밀번호 주소 */
   @NotBlank(message = "비밀번호를 입력해야합니다.")
   @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
   @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
