1a8760ef22078751f211da68284661947d6a9941
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-04-11 14:01:42 +0900

    User Domain 수정후 Test Case Doamin Sync

--- src/main/java/kr/flap/domain/model/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/flap/domain/model/common/exception/GlobalExceptionHandler.java
@@ -1,2 +1,22 @@
-package kr.flap.domain.model.common.exception;public class GlobalExceptionHandler {
+package kr.flap.domain.model.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    String errorMessage = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+            .reduce("", (message1, message2) -> message1 + "\n" + message2);
+
+    return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+  }
 }
--- src/main/java/kr/flap/domain/model/product/validation/ValidId.java
+++ src/main/java/kr/flap/domain/model/product/validation/ValidId.java
@@ -1,2 +1,17 @@
-package kr.flap.domain.model.product.validation;public class ValidId {
+package kr.flap.domain.model.product.validation;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+public class ValidId {
+
+  @NotNull
+  @Digits(integer = 15, fraction = 0)
+  private BigInteger id;
 }
