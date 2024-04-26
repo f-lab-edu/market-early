c98a818f0292ef183f6ca3a000ae2018937959ed
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-04-27 00:57:10 +0900

    [#8] Spring security Payment 프론트 부분 연계 설정 및 application 연동 설정 git 에서 제외

--- src/main/java/kr/flap/config/SecurityConfig.java
+++ src/main/java/kr/flap/config/SecurityConfig.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,9 +20,6 @@ import java.util.Arrays;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-  private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
@@ -40,7 +34,7 @@ public class SecurityConfig {
     http.authorizeHttpRequests((auth) -> auth
             .requestMatchers("/v1/users/**").permitAll()
             .requestMatchers("/v1/products/**").permitAll()
-            .requestMatchers("/v1/payments/**").authenticated()
+            .requestMatchers("/v1/payments/**").permitAll()
             .requestMatchers("/success").permitAll()
             .requestMatchers("/confirm").permitAll()
             .requestMatchers("/style.css").permitAll()
@@ -62,7 +56,8 @@ public class SecurityConfig {
             "https://static.toss.im",
             "https://pages.tosspayments.com",
             "https://polyfill-fe.toss.im",
-            "https://assets-fe.toss.im"
+            "https://assets-fe.toss.im",
+            "http://localhost:8080/v1/payments/**"
     ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -75,12 +70,5 @@ public class SecurityConfig {
     RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
     roleHierarchy.setHierarchy("ADMIN > USER");
     return roleHierarchy;
-  }
-
-  @Bean
-  public AuthenticationManager customAuthenticationManager() throws Exception {
-    AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder((ObjectPostProcessor<Object>) customUserDetailsService);
-    builder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    return builder.build();
   }
 }
