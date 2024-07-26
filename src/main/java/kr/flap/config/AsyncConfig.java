5a23e703decf8ca3ad88fb0e027a35b4f59786eb
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-07-26 15:35:38 +0900

    [#33] 마지막으로 최대한 스레드 수를 늘려본다

--- src/main/java/kr/flap/config/AsyncConfig.java
+++ src/main/java/kr/flap/config/AsyncConfig.java
@@ -9,9 +9,9 @@ public class AsyncConfig {
   @Bean(name = "taskExecutor")
   public ThreadPoolTaskExecutor taskExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(50);  // 기본 스레드 수
-    executor.setMaxPoolSize(200);  // 최대 스레드 수
-    executor.setQueueCapacity(500); // 작업 큐 크기
+    executor.setCorePoolSize(250);  // 기본 스레드 수
+    executor.setMaxPoolSize(380);  // 최대 스레드 수
+    executor.setQueueCapacity(1500); // 작업 큐 크기
     executor.setThreadNamePrefix("Async-");
     executor.initialize();
     return executor;
