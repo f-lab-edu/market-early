bbad4004643bf32b301ac91ce24f803ebd83e2aa
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-07-26 14:12:34 +0900

    [#33] 다시 동적으로 쓰레드 풀을 조정할수있게 수정

--- src/main/java/kr/flap/CurlyBackendApplication.java
+++ src/main/java/kr/flap/CurlyBackendApplication.java
@@ -3,9 +3,11 @@ package kr.flap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class CurlyBackendApplication {
 
   public static void main(String[] args) {
