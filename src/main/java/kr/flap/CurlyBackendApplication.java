a8361165565972b631c04922a0795eef680458a9
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-08-08 13:25:25 +0900

    Docker로 배포시 logging에 대한 시간 설정

--- src/main/java/kr/flap/CurlyBackendApplication.java
+++ src/main/java/kr/flap/CurlyBackendApplication.java
@@ -1,6 +1,7 @@
 package kr.flap;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
+@Slf4j
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
@@ -16,6 +18,7 @@ public class CurlyBackendApplication {
   @PostConstruct
   public void started() {
     TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    log.info("Current JVM timezone: " + java.util.TimeZone.getDefault().getID());
   }
 
   public static void main(String[] args) {
